d58aea49a22abfc2d3b6b1979f3c81f1b7dfca3d
tingtao.suntt <tingtao.suntt@alibaba-inc.com>
2017-11-21 20:18:12 +0800

    refine request and connect timeout

--- Aliyun/Log/requestcore.class.php
+++ Aliyun/Log/requestcore.class.php
@@ -610,8 +610,8 @@ class RequestCore
 		curl_setopt($curl_handle, CURLOPT_MAXREDIRS, 5);
 		curl_setopt($curl_handle, CURLOPT_HEADER, true);
 		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl_handle, CURLOPT_TIMEOUT, 5184000);
-		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 120);
+		curl_setopt($curl_handle, CURLOPT_TIMEOUT, 20);
+		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 5);
 		curl_setopt($curl_handle, CURLOPT_NOSIGNAL, true);
 		curl_setopt($curl_handle, CURLOPT_REFERER, $this->request_url);
 		curl_setopt($curl_handle, CURLOPT_USERAGENT, $this->useragent);
